019cc5408b74a52b8152c5aeedae175ab4d4beca
nagato <nagato46@gmail.com>
2013-08-08 20:48:11 +0900

    Refactored whole the project
    Refactored whole the project by using Xml TypeProvider.

--- NiconicoAPI/CommentClient.fs
+++ NiconicoAPI/CommentClient.fs
@@ -1,295 +1,137 @@
-﻿namespace RLib.NiconicoAPI
-#light
-#nowarn "40"
+﻿[<AutoOpen>]
+module CommentClient
 
-open RLib.NiconicoAPI.Xml
 open System
-open System.Diagnostics
-open System.Net.Sockets
-open System.IO
 open System.Text
+open FSharp.Net
+open System.Net
 
-///<summary>コメント投稿用キーの変更タイミング通知情報。</summary>
-///<param name="no">発言順番</param>
-///<param name="postkey">コメント投稿用キー</param>
-type PostKeyArgs(no:int, postkey:string) =
-    let mutable _no = no
-    let mutable _postkey = postkey
-    ///送信時に必要なキーの設定または取得
-    member x.PostKey
-        with get() = _postkey
-        and set v = _postkey <- v
-    ///最新の発言順番
-    member x.No
-        with get() = _no
-        and set v = _no <- v
-    ///<summary>POSTKEYのブロック番号の取得</summary>
-    ///<remarks>POSTKEYは発言順番が100の倍数になったら切り替わる。</remarks>
-    member x.BlockNo = _no/100
-    ///<summary>コメント投稿用キーの変更タイミング通知情報。</summary>
-    ///<param name="postkey">コメント投稿用キー</param>
-    new(postkey) =
-        new PostKeyArgs(0, postkey)
+do Net.ServicePointManager.ServerCertificateValidationCallback <- Net.Security.RemoteCertificateValidationCallback(fun _ _ _ _ -> true)
 
-///受信データの判別共用体
-type ReciveData =
-    | ConnectionStatus of Thread
-    | CommentResult of ChatResult
-    | Comment of Chat
-    | Unknown of string
+///コメント投稿用キーの変更タイミング通知情報。
+type PostKey = { mutable PostKey:string;  mutable No:int }
 
-///送受信データの判別共用体
-type CommentData =
-    ///受信エラー
-    | ReceiveNG of Exception * int * int
-    ///正常受信
-    | ReceiveOK of ReciveData * int * int
-    ///送信エラー
-    | SendNG of Exception * int * int
-    ///正常送信
-    | SendOK of string * int * int
 
-///<summary>ニコ生コメントサーバの為の非同期送受信を提供します。</summary>
-///<param name="setting">CommentClientの設定情報</param>
-type CommentClient (gps:GetPlayerStatus) =
-//#region メンバ
-    let _enc = Encoding.UTF8
-    let _commentThreadID = "<thread thread=\"{0}\" version=\"20061206\" res_from=\"-200\"/>"
-    let _commentSendTemp = @"<chat thread=""{0}"" ticket=""{1}"" vpos=""{2}"" postkey=""{3}"" mail=""{4}"" user_id=""{5}"" premium=""{6}"">{7}</chat>"
+///ニコ生コメントサーバの為の非同期送受信を提供します。
+type CommentClient (cc:CookieContainer,id) as __ =
+  /// ニコニコ動画API getplayerstatus を呼び出す。
+  let getplayerstatus (id:string) =
+    let getplayerstatus = sprintf "http://live.nicovideo.jp/api/getplayerstatus?v=%s" id
+    Http.Request(getplayerstatus, cookieContainer = cc)
+    |> GetPlayerStatus.Parse
 
-    let getThreadID() =
-        System.Threading.Thread.CurrentThread.ManagedThreadId
-    let _mainThreadID = getThreadID()
+  /// ニコニコ動画API getpostkey を呼び出す。
+  let getpostkey threadid no =
+    let getpostkey = sprintf "http://live.nicovideo.jp/api/getpostkey?thread=%d&block_no=%d" threadid (no/100)
+    let s = Http.Request(getpostkey, cookieContainer = cc)
+    s.[ s.IndexOf "=" + 1 .. ]
+  let mutable gps = getplayerstatus id
 
-    let _client = new TcpClient(gps.Ms.Addr, gps.Ms.Port)
-    let _stream = _client.GetStream()
+  let _client = new Sockets.TcpClient(gps.Ms.Addr, gps.Ms.Port)
+  let _stream = _client.GetStream()
 
-    let _threadID = gps.Ms.Thread
-    let _initTime = new TimeSpan()
-    let rec _come184seq =
-        seq { yield true; yield false; yield! _come184seq; }
-    let _come184 = _come184seq.GetEnumerator()
+  ///非同期送信イベント
+  let commentReceived = Event<CommentData>()
+  
+  let mutable _thread : Thread.DomainTypes.Thread option = None
+  let mutable _chat : Chat.DomainTypes.Chat option = None
+  let mutable _postkey = { PostKey = ""; No = 0 }
+  let mutable _recomment = ""
 
-    ///キャンセルトークン
-    let token = Async.DefaultCancellationToken
-    ///非同期送信イベント
-    let transceiverEvent = new Event<CommentData>()
-    ///非同期送信イベント
-    let changedPostKeyEvent = new Event<PostKeyArgs>()
-//#endregion
-
-//#region 副作用のメンバ
-    let _thread : Thread option ref = ref None
-    let _chat : Chat option ref = ref None
-    let _postkey = new PostKeyArgs("")
-    let _recomment = ref ""
-//#endregion
-
-    /// 送信文字列をbyte配列に変換してNULLを付け足す。
-    let toByte (comment:string) =
-        Array.append
-            <| _enc.GetBytes(comment)
-            <| Array.zeroCreate 1
+  //イニシャライズ時の設定
+  let onPostKeyChanged () =
+    /// 不正コメ番号の場合はステータスXMLから再取得する。
+    if _postkey.No = -1 then
+      gps <- getplayerstatus  gps.Stream.Id
+      _postkey.No <- gps.Stream.CommentCount 
+    _postkey.PostKey <- getpostkey  gps.Ms.Thread  _postkey.No 
             
-//#region 副作用のプロパティ
-    ///経過時間を取得する。
-    member x.ElapsedTime =
-        if _thread.Value.IsSome then
-            _thread.Value.Value.ElapsedTime
-        else _initTime
-    ///184の設定または取得する。
-    member x.Come184
-        with get () = _come184.Current
-        and set v =
-            if _come184.Current <> v then
-                _come184.MoveNext() |> ignore
+  member __.Cookie = cc
+  member __.GetStatus = gps
+  ///経過時間を取得する。
+  member __.ElapsedTime = _thread |> Option.map(fun __ -> DateTime.Now - XmlCommon.toDate (__.ServerTime - gps.Time))
+  member val Come184 = false with get,set
+  ///コメント受信イベントを提供する。
+  member __.CommentReceived = commentReceived.Publish
 
-    /// 受信データの判別結果を取得する。
-    member private x.GetReceiveData (text:string) =
-        if text.StartsWith("<thread") then
-            _thread := Some <| XmlReaders.ThreadReader(gps, text)
-            //POSTKEYの変更通知
-            if _chat.Value.IsNone then
-                changedPostKeyEvent.Trigger(_postkey)
-            ReciveData.ConnectionStatus _thread.Value.Value
-        elif text.StartsWith("<chat_result") then
-            let cr = XmlReaders.ChatResultReader text
-            //POSTKEYの変更通知（コメ番不正などによって計算が狂う場合があるので、コメ番を-1に置き換えて、イベント側で再取得させる。）
-            if cr.Status = ChatResultStatus.PostKeyError then
-                _postkey.No <- -1
-                changedPostKeyEvent.Trigger(_postkey)
-                x.ReSendComment()
-            ReciveData.CommentResult cr
-        elif text.StartsWith("<chat") then
-            _chat := Some <| XmlReaders.ChatReader(gps, _thread.Value.Value, text)
-            let no = _chat.Value.Value.No
-            _postkey.No <- no
-            //POSTKEYの変更通知
-            if 0 < no && no%100 = 0 then
-                changedPostKeyEvent.Trigger(_postkey)
-            ReciveData.Comment _chat.Value.Value
-        else
-            ReciveData.Unknown text
-//#endregion
+  /// 受信データの判別結果を取得する。
+  member private __.GetReceiveData text =
+    let (|StartsWith|_|) pat (input:string) = if input.StartsWith pat then Some () else None
+    match text with
+    | StartsWith "<thread" ->
+      _thread <- Some <| Thread.Parse text
+      if _chat.IsNone then onPostKeyChanged ()
+      ReciveData.ConnectionStatus _thread.Value
+    | StartsWith "<chat_result" ->
+      let cr = ChatResult.Parse text
+      // コメ番不正などによって計算が狂う場合があるので、コメ番を-1に置き換えて、イベント側で再取得させる。）
+      if enum cr.Status = ChatResultStatus.PostKeyError then
+        _postkey.No <- -1
+        onPostKeyChanged ()
+        __.ReSendComment ()
+      ReciveData.CommentResult cr
+    | StartsWith "<chat" ->
+      let chat = Chat.Parse text
+      _chat <- Some chat
+      _postkey.No <- chat.No
+      if 0 < chat.No && chat.No%100 = 0 then onPostKeyChanged ()
+      ReciveData.Comment chat
+    | _ -> ReciveData.Unknown text
 
-    ///送受信イベントを提供する。
-    [<CLIEventAttribute>]
-    member x.Transceiver = transceiverEvent.Publish
-    ///PostKeyの変更タイミングイベントを提供する。
-    [<CLIEventAttribute>]
-    member x.ChangedPostKeyEvent = changedPostKeyEvent.Publish
+  /// コメントを非同期で送信する。
+  member __.Send (comment:string) =
+    async {
+    commentReceived.Trigger <|
+      try
+        let b = [| yield! Encoding.UTF8.GetBytes comment; yield 0uy |] 
+        _stream.Write(b, 0, b.Length)
+        CommentData.SendOK comment
+      with ex -> CommentData.SendNG ex
+    } |> Async.Start
 
-    ///CommentClientSettingで設定したスレッドIDを送信する。
-    member x.SendThreadId () =
-        x.Send
-            <| String.Format(_commentThreadID, _threadID)
-            
-    ///<summary>コメントXMLに変換後に送信する。</summary>
-    ///<param name="comment">コメント</param>
-    member x.SendComment comment =
-        if _thread.Value.IsSome then
-            _recomment := comment
+  ///CommentClientSettingで設定したスレッドIDを送信する。
+  member __.SendThreadId () =
+    sprintf """<thread thread="%d" version="20061206" res_from="-200"/>""" gps.Ms.Thread
+    |> __.Send
+      
+  ///コメントXMLに変換後に送信する。
+  member __.SendComment comment =
+    if _thread.IsSome then
+      _recomment <- comment
+      let comment = Web.HttpUtility.HtmlDecode comment
+      let vpos = UnixTime.FromTimeSpanToUnixTime __.ElapsedTime.Value 
+      let mail = if __.Come184 then @"mail=""184""" else ""
+      sprintf """<chat thread="%d" ticket="%s" vpos="%d" postkey="%s" %s user_id="%s" premium="%d">%s</chat>"""
+        gps.Ms.Thread  _thread.Value.Ticket  vpos  _postkey.PostKey  mail  gps.User.UserId  gps.User.IsPremium  comment
+      |> __.Send 
+  ///前回のコメントを再送信する。
+  member __.ReSendComment() = __.SendComment _recomment
 
-            let comment = Commons.toHtmlDecode comment
+  ///永続的に非同期コメント受信を行う。
+  member __.ReceiveStart() =
+    // 永続非同期受信を行う
+    async {
+    let nextdata = ref [||]
+    while true do
+      try
+        let! data = _stream.AsyncRead 1
+        nextdata := Array.append !nextdata data
+        if data.[0] = 0uy then
+          let data = Array.filter ((<>)0uy) !nextdata |> Encoding.UTF8.GetString |> __.GetReceiveData
+          CommentData.ReceiveOK data |> commentReceived.Trigger
+          nextdata := [||]
+      with ex -> CommentData.ReceiveNG ex |> commentReceived.Trigger
+    } |> Async.Start
 
-            let t = _thread.Value.Value
-            let xml =
-                String.Format(
-                    _commentSendTemp,
-                    _threadID,
-                    t.Ticket,
-                    UnixTime.FromTimeSpanToUnixTime t.ElapsedTime,
-                    _postkey.PostKey,
-                    (if x.Come184 then "184" else ""),
-                    gps.User.UserID,
-                    gps.User.IsPremium,
-                    comment)
-            let xml = xml.Replace("mail=\"\"","")
-            Debug.WriteLine(sprintf "%s" xml)
-            x.Send xml
-    ///<summary>前回のコメントを再送信する。</summary>
-    member x.ReSendComment() = x.SendComment !_recomment
+  member __.Start () =
+    __.ReceiveStart()
+    __.SendThreadId()
 
-    ///<summary>コメントを非同期で送信する。</summary>
-    ///<param name="comment">コメント</param>
-    member x.Send (comment:string) =
-        let asyncSend =
-            async {
-                let threadID = getThreadID()
-                try
-                    let b = toByte comment
-                    do! _stream.AsyncWrite(b, 0, b.Length)
-                    transceiverEvent.Trigger
-                        <| CommentData.SendOK(comment, _mainThreadID, threadID)
-                with
-                | ex ->
-                    transceiverEvent.Trigger
-                        <| CommentData.SendNG(ex, _mainThreadID, threadID)
-            }
-        Async.Start(asyncSend, token)
-
-    ///<summary>永続的に非同期コメント受信を行う。</summary>
-    member x.ReceiveStart() =
-        // 永続非同期受信を行う
-        let rec loop arr = async {
-            let threadID = getThreadID()
-            try
-                let! data = _stream.AsyncRead(1)
-                let nextdata = Array.append arr data
-                match data with
-                | [| 0uy |] ->
-                    //コメントに変換
-                    let text =
-                        nextdata
-                        |> Array.filter ((<>)0uy)
-                        |> _enc.GetString
-
-                    Debug.WriteLine(sprintf "%s" text)
-                    //各取得 XML で判別する。
-                    let data = x.GetReceiveData text
-
-                    //受信イベントの発行
-                    transceiverEvent.Trigger
-                        <| CommentData.ReceiveOK(data, _mainThreadID, threadID)
-                | _ ->
-                    //次の受信待ちを呼び出す。
-                    do! loop nextdata
-            with
-            | ex ->
-                transceiverEvent.Trigger
-                    <| CommentData.ReceiveNG(ex, _mainThreadID, threadID)
-
-            do! loop Array.empty
-        }
-        Async.Start(loop Array.empty, token)
-
-    interface IDisposable with
-        /// CommentClientに割り当てられたリソースをすべて解放する。
-        member x.Dispose() =
-            Async.CancelDefaultToken()
-            try
-                _stream.Dispose()
-                _client.Close()
-                // 必要？ 念のため相手側のサーバのコネクションを
-                // 完全に消すために使用。
-                GC.Collect()
-            with | _ -> ()
-
-///<summary>コメントを非同期で送信する。</summary>
-///<param name="gps">コメント</param>
-///<param name="gps">コメント</param>
-type CommentClientHelper (cookie, id:string) =
-    let _nhttp = new NiconicoHttp(cookie)
-    let _gps = _nhttp.GetStatus(id)
-    let _client = new CommentClient(_gps)
-    
-    //イニシャライズ時の設定
-    do
-        _client.ChangedPostKeyEvent.Add(fun args ->
-            /// 不正コメ番号の場合はステータスXMLから再取得する。
-            let (threadid,blockno) =
-                if args.No = -1 then
-                    let _gps = _nhttp.GetStatus(_gps.Stream.Id)
-                    _gps.Ms.Thread, (_gps.Stream.CommentCount/100)
-                else
-                    _gps.Ms.Thread, args.BlockNo
-
-            let postkey = _nhttp.GetPostkey blockno threadid
-            Debug.WriteLine(
-                sprintf "[%d,%d] POSTKEYのブロックNoが変更されました。"
-                    <| args.No
-                    <| args.BlockNo)
-            args.PostKey <- postkey
-        )
-
-    ///ステータスXMLを取得する。
-    member x.GetStatus = _gps
-    ///コメントサーバクライアントを取得する。
-    member x.Client = _client
-    ///経過時間を取得する。
-    member x.ElapsedTime = _client.ElapsedTime
-    ///184の設定または取得する。
-    member x.Come184
-        with get() = _client.Come184
-        and set v = _client.Come184 <- v
-
-    ///送受信イベントを提供する。
-    [<CLIEventAttribute>]
-    member x.Transceiver = _client.Transceiver  // イベントの橋渡し
-
-    ///<summary>コメントを送信する。</summary>
-    ///<param name="comment">コメント</param>
-    member x.Send comment =
-        _client.SendComment comment
-        
-    ///<summary>コメントサーバへの接続を開始します。</summary>
-    ///<remarks>開始前と同時にコメント受信が行われる為、開始前にイベントを設定する必要があります。</remarks>
-    member x.Start () =
-        _client.ReceiveStart()
-        _client.SendThreadId()
-
-    interface IDisposable with
-        /// CommentClientに割り当てられたリソースをすべて解放する。
-        member x.Dispose() =
-            let i = _client :> IDisposable
-            i.Dispose()
+  interface IDisposable with
+    member __.Dispose() =
+      Async.CancelDefaultToken()
+      try
+        _stream.Dispose()
+        _client.Close()
+        GC.Collect() // 必要？ 念のため相手側のサーバのコネクションを完全に消すために使用。
+      with _ -> ()
---
+++ NiconicoAPI/Misc.fs
@@ -0,0 +1,64 @@
+﻿[<AutoOpen>]
+module Misc
+
+open System
+open System.Reflection
+
+[<assembly: AssemblyTitle "RLib.NiconicoAPI";
+  assembly: AssemblyDescription "ニコニコ動画簡易API";
+  assembly: AssemblyProduct "RLib.NiconicoAPI";
+  assembly: AssemblyCopyright "Copyright© 2012 regerege  All Rights Reserved.";
+  assembly: AssemblyVersion "0.1.2.*">] ()
+
+
+///UNIX時間と日本時間の変換をサポート
+module UnixTime =
+  /// 日本時間とUNIX時間の差
+  let UnixEpoch = DateTime(1970, 1, 1, 9, 0, 0, DateTimeKind.Local)
+  let ticks = 100000L
+
+  ///UNIX時間をDateTimeに変換する。
+  let FromUnixTime time = UnixEpoch.AddSeconds time
+  ///UNIX時間をTimeSpanに変換する。
+  let FromUnixTimeToTimeSpan time = TimeSpan (time * ticks)
+  ///DateTimeをUNIX時間に変換する。
+  let ToUnixTime (date:DateTime) = (date - UnixEpoch).TotalSeconds
+  ///TimeSpanをUNIX時間に変換する。
+  let FromTimeSpanToUnixTime (ts:TimeSpan) = ts.Ticks / ticks
+
+
+module XmlCommon =
+  let toInt n = try Int32.Parse n with _ -> -1
+  let toDate (time:int) = string time |> Double.Parse |> UnixTime.FromUnixTime 
+  let toTimeSpan str = try Int64.Parse str |> UnixTime.FromUnixTimeToTimeSpan with _ -> TimeSpan.Zero
+
+
+open FSharp.Data
+type ChatResult = XmlProvider<"""<chat_result thread="1288164172" status="0" no="1920"/>""">
+type Chat = XmlProvider<"""<chat thread="1288360965" no="1" vpos="55716" date="1375930076" date_usec="778378" user_id="aaa" premium="1">わこつー</chat>""">
+type GetPlayerStatus = XmlProvider<"""<?xml version="1.0" encoding="utf-8"?><getplayerstatus status="ok" time="1375932769"><stream><id>lv148064383</id><title>ひよっこ創作人の雑談(2791枠目)。</title><description>主もリスナも一歩前進するために必要な、休息をするための放送。主はアマチュアの創作者です。　創作に関する話題は歓迎です。この放送の詳細については、コミュプロフを参</description><provider_type>community</provider_type><default_community>co1741635</default_community><international>13</international><is_owner>0</is_owner><owner_id>6251677</owner_id><owner_name>みるきあ</owner_name><is_reserved>0</is_reserved><watch_count>10</watch_count><comment_count>57</comment_count><base_time>1375931334</base_time><open_time>1375931334</open_time><start_time>1375931342</start_time><end_time>1375933142</end_time><is_rerun_stream>0</is_rerun_stream><bourbon_url>http://live.nicovideo.jp/gate/lv148064383?sec=nicolive_crowded&amp;sub=watch_crowded_0_community_lv148064383_onair</bourbon_url><full_video>http://live.nicovideo.jp/gate/lv148064383?sec=nicolive_crowded&amp;sub=watch_crowded_0_community_lv148064383_onair</full_video><after_video></after_video><before_video></before_video><kickout_video>http://live.nicovideo.jp/gate/lv148064383?sec=nicolive_oidashi&amp;sub=watchplayer_oidashialert_0_community_lv148064383_onair</kickout_video><twitter_tag>#co1741635</twitter_tag><danjo_comment_mode>0</danjo_comment_mode><infinity_mode>0</infinity_mode><archive>0</archive><press><display_lines>-1</display_lines><display_time>-1</display_time><style_conf></style_conf></press><plugin_delay></plugin_delay><plugin_url></plugin_url><plugin_urls/><allow_netduetto>1</allow_netduetto><nd_token>fcacad3eb354acdaee04f2a476d1f71649c4a276</nd_token><ng_scoring>0</ng_scoring><header_comment>0</header_comment><footer_comment>0</footer_comment><split_bottom>0</split_bottom><split_top>0</split_top><background_comment>0</background_comment><font_scale></font_scale><comment_lock>0</comment_lock><telop><enable>0</enable></telop><contents_list><contents id="main" disableAudio="0" disableVideo="0" start_time="1375931335">rtmp:rtmp://nlpoca29.live.nicovideo.jp:1935/publicorigin/130808_12_1/,lv148064383?1375932769:30:26d4ae9b338cfb71</contents></contents_list><is_priority_prefecture></is_priority_prefecture></stream><user><user_id>someuserid</user_id><nickname>ながと</nickname><is_premium>1</is_premium><userAge>26</userAge><userSex>1</userSex><userDomain>jp</userDomain><userPrefecture>64</userPrefecture><userLanguage>ja-jp</userLanguage><room_label>co1741635</room_label><room_seetno>8</room_seetno><is_join>1</is_join><twitter_info><status>disabled</status><screen_name></screen_name><followers_count>0</followers_count><is_vip>0</is_vip><profile_image_url>http://a1.twimg.com/sticky/default_profile_images/default_profile_6_normal.png</profile_image_url><after_auth>0</after_auth><tweet_token>6c96bb5ba64ea460cef9760d56bce5e02f4c6892</tweet_token></twitter_info></user><rtmp is_fms="1" rtmpt_port="80"><url>rtmp://nleaf11.live.nicovideo.jp:1935/liveedge/live_130808_12_1</url><ticket>67286:lv148064383:0:1375932769:5a46ecfad27496ea</ticket></rtmp><ms><addr>msg103.live.nicovideo.jp</addr><port>2808</port><thread>1288363774</thread></ms><tid_list/><twitter><live_enabled>0</live_enabled><vip_mode_count>10000</vip_mode_count><live_api_url>http://watch.live.nicovideo.jp/api/</live_api_url></twitter><player><error_report>1</error_report></player><marquee><category>一般(その他)</category><game_key>afd55ec9</game_key><game_time>1375932769</game_time><force_nicowari_off>0</force_nicowari_off></marquee></getplayerstatus>""">
+type Thread = XmlProvider<"""<thread resultcode="0" thread="1288366260" last_res="260" ticket="0x8439b00" revision="1" server_time="1375933886" />""">
+
+
+///受信データの判別共用体
+type ReciveData =
+  | ConnectionStatus of Thread.DomainTypes.Thread
+  | CommentResult of ChatResult.DomainTypes.ChatResult
+  | Comment of Chat.DomainTypes.Chat
+  | Unknown of string
+
+///送受信データの判別共用体
+type CommentData = 
+  | ReceiveNG of Exception 
+  | ReceiveOK of ReciveData 
+  | SendNG of Exception 
+  | SendOK of string
+
+/// 送信結果ステータス
+type ChatResultStatus = 
+  | Success = 0 
+  | Failure = 1 
+  | ThreadIdError = 2 
+  | TicketError = 3 
+  | PostKeyError = 4 
+  | CommentBlockError = 5
